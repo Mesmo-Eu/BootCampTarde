5f03b20d9a01a371aea59161fe358c55a1832559
Marcos <marcos.cruz@ufvjm.edu.br>
2023-01-25 15:14:13 -0300

    alou

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -1,12 +1,36 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers;
 
-public class TarefasController: Controller
+public class TarefasController : Controller
 { 
+    public List<TarefaViewModel> listaDeTarefas {get; set;}
+    public TarefasController()
+    {
+        listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel(){Id = 1, Titulo = "Escovar os dentes"},
+            new TarefaViewModel(){Id = 2, Titulo = "Arrumar a cama"},
+            new TarefaViewModel(){Id = 3, Titulo = "Por o lixo para fora", Descricao ="somente as terças"}
+        };
+
+    }
+    
     public IActionResult Create()
     {
         return View();
+    }
+
+    public IActionResult Index()
+    {
+      return View(listaDeTarefas);  
+    }
+
+    public IActionResult Details(int id)
+    {
+        var tarefa = listaDeTarefas.Find(tarefa => tarefa.Id == id);
+        return View(tarefa);
     }
 }
