d7b9ec520fd00bbb5163444a9c559035c85d4676
Marcos <marcos.cruz@ufvjm.edu.br>
2023-01-25 16:50:43 -0300

    backup

---
+++ Tarefas/src/Tarefas.DAO/TarefaDAO.cs
@@ -0,0 +1,41 @@
+using Dapper;
+using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+
+namespace Tarefas.DAO
+{
+    public class TarefaDAO
+    {
+        private string DataSourceFile => Environment.CurrentDirectory + "TarefasDB.sqlite";
+        public SQLiteConnection Connection => new SQLiteConnection("Datasource=" + DataSourceFile);
+    
+    public TarefaDAO()
+    {
+        if(!File.Exists(DataSourceFile))
+        {
+            CreateDatabase();
+        }
+    }
+
+    public void CreateDatabase()
+    {
+        using(var con = Connection)
+        {
+        con.Open();
+        con.Execute(
+            @"CREATE TABLE Tarefa
+            (
+                Id          integer primary key autoincrement,
+                Titulo      varchar(100) not null,
+                Descricao   varchar not null (100))"
+        );
+        }
+        
+    }
+    
+    
+    }
+}
+
